457d880a8b0993b97ac43aad2d874be01b6de316
RaphaelMardine <rafamardine@hotmail.com>
2022-08-25 11:25:34 -0300

    is loading

--- src/components/B2BTutorial/index.tsx
+++ src/components/B2BTutorial/index.tsx
@@ -14,6 +14,7 @@ import { json } from 'stream/consumers'
         data: Daum[]
         dataB?: any
         meta?: Meta
+        isLoading? : any
       }
       
       export interface Daum {
@@ -90,23 +91,23 @@ import { json } from 'stream/consumers'
         total: number
       }
       
-const B2BLeftContent = ({ data, dataB, setId, setIdVideo }:B2BTutorial) => {
+const B2BLeftContent = ({ data, dataB, setId, setIdVideo, isLoading }:B2BTutorial) => {
     return (         
         <section className="B2BLeftContent">
             <Switch />
             <Search />
-            <ModuleAccordion data={data} dataB={dataB} setId={setId} setIdVideo={setIdVideo} />
+            <ModuleAccordion data={data} dataB={dataB} setId={setId} setIdVideo={setIdVideo} isLoading={isLoading} />
         </section>
     )
 }
 
-const B2BRightContent = ({ data, responseVideobyId }:B2BTutorial) => {
-    console.log(responseVideobyId, 'responseVideobyId')
-    return  (           
+const B2BRightContent = ({ dataB }:B2BTutorial) => {
+  console.log(dataB)
+    return  (
         <section className="B2BRightContent">
-            <ContentDescription responseVideobyId={responseVideobyId}/>
+            <ContentDescription responseVideobyId={dataB}/>
                 <div className="B2BPlayerContent">
-                    <PlayerVideo responseVideobyId={responseVideobyId}/>
+                    <PlayerVideo responseVideobyId={dataB}/>
                 </div>
         </section>
     ) 
@@ -121,8 +122,8 @@ const Switch = () => {
             </label>
             <div className="B2BContainerSpan">
             <span>Reprodução automática</span>
-            </div>
             <img src={closeIcon} alt="Close content" onClick={() => {alert("Sai daqui, não terminei essa feature!")}} />
+            </div>
         </div>
     )
 }
@@ -137,24 +138,31 @@ const Search = () => {
 }
 
 
-const AccordionList = ({ data, setId, dataB, setIdVideo }:B2BTutorial) => {
-    const [accordionId, setAccordionId] = useState(0)
+const AccordionList = ({ data, setId, dataB, setIdVideo, isLoading }:B2BTutorial) => {
+    const [accordionId, setAccordionId] = useState(1)
     const [selected, setSelected] = useState('')
     return (
     <>
+    
     {//@ts-ignore
     data?.data?.map((item:Daum) => {
         return (
-    <li className={`B2BContentModule  ${accordionId === item.id && selected}`}>
-        <li className="B2BContentModuleName" onClick={() => {setAccordionId(item.id); setSelected('ContainerSelected'); setId(item.id)}} key={item.id}>
-            <p>Módulo</p> <p>{'3:24'}</p>
+    <li className={`B2BContentModule   ${accordionId === item.id && selected}`}>
+        <div className="B2BContainerContentReference">
+        <li className="B2BContentModuleName" onClick={() => {setAccordionId(item.id); setSelected('ContainerSelected'); setId(item.id);}} key={item.id}>
+            <p>Módulo</p> 
+            {/* <p>{'3:24'}</p> */}
         </li>
         <p className="B2BContentModuleTitle">{item.attributes.title}</p>
+        </div>
         {accordionId === item.id && 
             <li className="B2BAccordionContent ContentSelected" key={item.id}>
               {dataB?.data?.attributes?.checkout_videos?.data?.map((row:any)=> {
-                return <p onClick={() => {setIdVideo(row?.id)}}>{row?.attributes?.title}</p>
-              })}
+                return (
+                <li className="B2BAccordionContentSelected"> <p onClick={() => {setIdVideo(row?.id)}}>
+                  {row?.attributes?.title}</p> <p>{row.attributes.video_duration}</p> 
+                </li>
+              )})}
            </li>
         }
     </li> 
@@ -163,10 +171,10 @@ const AccordionList = ({ data, setId, dataB, setIdVideo }:B2BTutorial) => {
    )
 }
 
-const ModuleAccordion = ({ data, dataB, setId, setIdVideo }:B2BTutorial) => {
+const ModuleAccordion = ({ data, dataB, setId, setIdVideo, isLoading }:B2BTutorial) => {
     return  (
     <ul className="B2BContainerContentModule">
-        <AccordionList data={data} dataB={dataB} setId={setId} setIdVideo={setIdVideo} />
+        <AccordionList data={data} dataB={dataB} setId={setId} setIdVideo={setIdVideo} isLoading={isLoading}/>
     </ul>
   )
 }
@@ -174,32 +182,57 @@ const ModuleAccordion = ({ data, dataB, setId, setIdVideo }:B2BTutorial) => {
 const ContentDescription = ({responseVideobyId}:any) => {
     return  (
         <div className="B2BContentDescription">
+          <div className="B2BContainerHeader">
             <h1>{responseVideobyId?.data?.attributes?.title}</h1>
-            <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
+            <img src={closeIcon} alt="Close content" onClick={() => {alert("Sai daqui, não terminei essa feature!")}} />
+          </div>
+            <p>{responseVideobyId?.data?.attributes?.description}</p>
         </div>
     )
 }
 
 const PlayerVideo = ({responseVideobyId}:any) => {
-    return(
-    <Player controls={false}>
+  const urlVideo = responseVideobyId != undefined ? `${responseVideobyId?.data?.attributes?.video?.data?.attributes?.url}` : ''
+  console.log(urlVideo)
+  console.log(responseVideobyId, '===================')
+  return (
+    <Player controls={false} autoplay playbackEnded>
             <Video poster={responseVideobyId?.data?.attributes?.video?.data?.attributes?.name}>
                 <source
-                   data-src={responseVideobyId?.data?.attributes?.video?.data?.attributes?.url}
+                   data-src={responseVideobyId != undefined ? responseVideobyId?.data?.attributes?.video?.data?.attributes?.url : ''}
                    type="video/mp4"
-                />
+                   />
             </Video>
         <DefaultUi />
     </Player>
   )
 }
+ function useMediaQuery(query:any) {
+  const [matches, setMatches] = useState(false);
 
-export const B2BTutorialClass = () => {
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => {
+      setMatches(media.matches);
+    };
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [matches, query]);
+
+  return matches;
+}
+
+export const B2BTutorialClass = (query:any) => {
   const [responseCMS, setResponseCMS] = useState([])
   const [id, setId] = useState(1)
   const [idVideo, setIdVideo] = useState(1)
   const [responseCMSbyId, setResponseCMSbyId] = useState([])
   const [responseVideobyId, setResponseVideobyId] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  let isPageWide = useMediaQuery('(max-width: 880px)')
 
     var myHeaders = new Headers();
     myHeaders.append("Authorization", "Bearer 6a32403a7e2fffd6096b231742d29e755b21850f0d3e81d31d4683f8d5d982210f938f639d8a8a1905c97bf45ddb82241b91b71e1f0e9b58be488279aa0fc06788a2b30023e89f1f2cb846493a4e4b8e79e011f51debf546a6db929f7f42e2b1a9fd033388ce5ba0d63c521104c000b0cf01d757ec64ab9847d7524a2edd9dd4");
@@ -215,23 +248,26 @@ export const B2BTutorialClass = () => {
         .then(data => setResponseCMS(data))
       },[])
       useEffect(() => {
+        setIsLoading(true)
         fetch(`http://localhost:1337/api/checkout-modules/${id}`, requestOptions)
         .then(response => response.json())
-        .then(data => setResponseCMSbyId(data))
+        .then(data => {setResponseCMSbyId(data); setIsLoading(false)})
+       
       },[id])
       useEffect(() => {
         fetch(`http://localhost:1337/api/checkout-videos/${idVideo}`, requestOptions)
         .then(response => response.json())
         .then(data => setResponseVideobyId(data))
       },[idVideo])
-      console.log(responseVideobyId, 'responseVideobyId')
+
     return (
          <>
             <div className="B2BContainer">
                 <div className="B2BContentContainer">
                     <div className="Flex">
+                      {/* {isLoading ? 'aaa' : 'bbbb'} */}
                         <B2BRightContent data={responseCMS} dataB={responseVideobyId} />
-                        <B2BLeftContent data={responseCMS} dataB={responseCMSbyId} setId={setId} setIdVideo={setIdVideo} />
+                        <B2BLeftContent data={responseCMS} dataB={responseCMSbyId} setId={setId} setIdVideo={setIdVideo} isLoading={isLoading} />
                     </div>
                 </div>
             </div>
